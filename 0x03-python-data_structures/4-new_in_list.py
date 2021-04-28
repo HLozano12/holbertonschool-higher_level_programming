be248b94c917d576bd5873869da838c002a81ccc
HLozano12 <HLR8503@gmail.com>
2021-04-28 22:50:37 +0000

    Pep update

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    B_list = my_list.copy()
+    if idx < 0 or idx > len(my_list):
+        return(B_list)
+    for h in range(len(B_list)):
+        if h == idx:
+            B_list[h] = element
+            return(B_list)
