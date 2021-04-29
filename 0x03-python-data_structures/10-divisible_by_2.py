ca00f0bf9a5708179491e7f1116028d44b2d3156
HLozano12 <HLR8503@gmail.com>
2021-04-29 05:32:21 +0000

    Divide True/False

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    H_list = []
+    for h in my_list:
+        if h % 2 == 0:
+            H_list.append(True)
+        else:
+            H_list.append(False)
+        return h_list
