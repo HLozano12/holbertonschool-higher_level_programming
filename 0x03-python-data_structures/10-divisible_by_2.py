7da1fe9cc4b2c0e516f468b8afcf2815066ee862
HLozano12 <HLR8503@gmail.com>
2021-04-29 07:01:15 +0000

    index change

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if len(my_list) == 0:
-        return None
-    H_list = []
-    for h in my_list:
-        if h % 2 == 0:
-            H_list.append(True)
-        else:
-            H_list.append(False)
-        return H_list
+    h_list = []
+    if my_list:
+        for h in my_list:
+            if h % 2 == 0:
+                h_list.append(True)
+            else:
+                h_list.append(False)
+    return (h_list)
