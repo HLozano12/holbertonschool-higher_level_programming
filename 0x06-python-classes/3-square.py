390ce34d0a001b1d6af7a362df0eb2efe97dc6e2
HLozano12 <HLR8503@gmail.com>
2021-05-06 19:41:42 +0000

    Private Attribue

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A Defined Square Class"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0):
+        """private instance"""
+        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+    def area(self):
+        """Public Instance Method"""
+        return self.__size **2
