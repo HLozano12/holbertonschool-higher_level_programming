6c5c94df48aac66419d942019d13d39e92b9f88a
HLozano12 <HLR8503@gmail.com>
2021-05-06 18:50:32 +0000

    1 square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A Defined Square Class"""
+
+
+class Square:
+    """Single class"""
+    def __init__(self, size):
+        """Private attribute"""
+        self.__size = size
