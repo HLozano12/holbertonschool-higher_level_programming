d5f4fe3316c3bab54ec115b214c34a712e597476
HLozano12 <HLR8503@gmail.com>
2021-05-09 04:12:07 +0000

    Remove Underscores

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,8 +8,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Private Instance Attributes"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -19,9 +19,9 @@ class Rectangle:
     def width(self, value):
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
@@ -31,6 +31,6 @@ class Rectangle:
     def height(self, value):
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
