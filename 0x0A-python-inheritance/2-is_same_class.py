a4ffa3d9054cbd6ec919268cbdc8772acde42d8a
HLozano12 <HLR8503@gmail.com>
2021-05-11 21:11:56 +0000

    If a is int

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Our Module"""
+
+
+def is_same_class(obj, a_class):
+    """our Prototype to check if same class"""
+
+    if type(obj) is not a_class:
+        return False
+
+    return True
