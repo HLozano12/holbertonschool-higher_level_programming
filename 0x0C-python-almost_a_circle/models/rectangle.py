283bfe5f5c43a323cc6a3c97f3cafbca6a0751c4
HLozano12 <HLR8503@gmail.com>
2021-05-15 22:22:33 +0000

    Task2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,13 +2,13 @@
 """Given Module"""
 
 from models.base import Base
-
+"""Import"""
 
 class Rectangle(Base):
     """Rect inherits child base"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-
+        """Class constructor"""
         self.height = height
         self.width = width
         self.x = x
