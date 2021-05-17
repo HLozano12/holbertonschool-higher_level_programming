a211f24344d9efe7eb6b26da046da1e7c13a86eb
HLozano12 <HLR8503@gmail.com>
2021-05-17 18:32:14 +0000

    typo

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -92,7 +92,7 @@ class Rectangle(Base):
         """Setter for y"""
 
         if type(value) is not int:
-            raise TypeError("y must be integer")
+            raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
