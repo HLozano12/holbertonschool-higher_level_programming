692cadcc9158e3d176c39544a3f6a73e5e21002d
HLozano12 <HLR8503@gmail.com>
2021-05-26 23:52:05 +0000

    Records in Tables update

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,9 @@
 -- Create table second_table in database
 -- multiple rows id, name, score
 -- SELECT and SHOW not allowed
-CREATE TABLE IF NOT EXIST second_table (id INT, name VARCHAR(256), scrore INT);
+CREATE TABLE IF NOT EXIST second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT);
 INSERT INTO second_table (id, name, score)
     VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
