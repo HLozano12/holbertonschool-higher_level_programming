40657d95e26ab8b306667944bfb24daf5270fce0
HLozano12 <HLR8503@gmail.com>
2021-05-26 23:55:30 +0000

    Realign

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,8 +2,14 @@
 -- multiple rows id, name, score
 -- SELECT and SHOW not allowed
 CREATE TABLE IF NOT EXIST second_table (
-    id INT,
-    name VARCHAR(256),
-    score INT);
-INSERT INTO second_table (id, name, score)
-VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
+        id INT,
+        name VARCHAR(256),
+        score INT
+);
+INSERT INTO second_table 
+        (id, name, score)
+VALUES
+        (1, "John", 10),
+        (2, "Alex", 3),
+        (3, "Bob", 14),
+        (4, "George", 8);
