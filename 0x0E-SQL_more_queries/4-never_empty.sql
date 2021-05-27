62fdc452f4329afb62264cbe10088de190b754ba
HLozano12 <HLR8503@gmail.com>
2021-05-27 21:33:10 +0000

    Spelling Error

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -2,4 +2,4 @@
 -- descp of id INT with default value 1
 -- name is VARCHAR(256)
 -- if already exist, continue
-CREATE TABLE IF NOT EXIST id_not_null (id INT DEFAULT 1, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
