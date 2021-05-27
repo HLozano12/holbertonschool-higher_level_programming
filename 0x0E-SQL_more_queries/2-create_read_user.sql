a5378bd494a635ddacc9321fbf93732e1335e585
HLozano12 <HLR8503@gmail.com>
2021-05-27 20:59:05 +0000

    type-o

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,6 +3,6 @@
 -- User should have SELECT priviledges
 -- have pswd user_0d_2_pwd
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USE IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 GRANT USAGE ON *.* TO user_0d_2@localhost;
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
