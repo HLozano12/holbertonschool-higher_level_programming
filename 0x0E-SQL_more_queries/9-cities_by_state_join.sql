bee121b4d751f3d9f3e716ec278cd192f0281835
HLozano12 <HLR8503@gmail.com>
2021-05-27 23:43:15 +0000

    typo

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,7 @@
 -- list all cities contained in hbtn_0d_usa
 -- record shold display cities.id - cities.name - states.name
 -- stored in asc by cities.id
-SELECT citites.id AS id,  cities.name AS name, states.name AS name
+SELECT cities.id AS id,  cities.name AS name, states.name AS name
 FROM cities INNER JOIN states
 ON cities.state_id = states.id
 ORDER BY cities.id ASC;
