d8ca1bd2e8bb382232b0eb2e59788ed8ab74f276
HLozano12 <HLR8503@gmail.com>
2021-05-27 22:46:09 +0000

    syntax layout?

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -5,5 +5,8 @@
 -- ascending cities.id
 SELECT cities.id, cities.name
 FROM cities
-WHERE cities.state_id IN (SELECT states.id, FROM states, WHERE states.name = 'California')
+WHERE cities.state_id IN (
+SELECT states.id
+FROM states
+WHERE states.name = 'California')
 ORDER BY cities.id ASC;
