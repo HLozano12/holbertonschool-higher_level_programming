1792347a31eca1d417cdff19d7af761de32fb314
HLozano12 <HLR8503@gmail.com>
2021-05-27 20:38:16 +0000

    iDENTY SET UP

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,6 +2,5 @@
 -- user_0d_1 should have privleges
 -- user pswrd should be set to user_0d_1_pwd
 -- If exist, not fail
-CREATE USER IF NOT EXISTS user_0d_1@localhost;
+CREATE USER IF NOT EXISTS 'user_0d_1@localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
-IDENTIFIED BY 'user_0d_1_pwd';
