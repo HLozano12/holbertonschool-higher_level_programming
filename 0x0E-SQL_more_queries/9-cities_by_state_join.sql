1b090e5d92289fc6ed9b7fd00e647e313e6f3b71
HLozano12 <HLR8503@gmail.com>
2021-05-27 23:38:30 +0000

    Inmplement AS

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,8 +1,7 @@
 -- list all cities contained in hbtn_0d_usa
 -- record shold display cities.id - cities.name - states.name
 -- stored in asc by cities.id
-SELECT citites.id, cities.name, states.name
-FROM cities
-INNER JOIN states
+SELECT citites.id AS id,  cities.name AS name, states.name AS name
+FROM cities INNER JOIN states
 ON cities.state_id = states.id
 ORDER BY cities.id ASC;
