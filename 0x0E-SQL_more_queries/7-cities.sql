e55c59a287a1139a4bd40accb3b7f095dae204fb
HLozano12 <HLR8503@gmail.com>
2021-05-27 22:11:37 +0000

    Foreign Key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- create db hbtn_0d_usa with table cities in it
+-- id INT, auto generate not NULL
+-- state_id INT, not NULL and must be FOREIGN KEY
+-- Foreign key must reference ID of STATES talbe
+-- name is still VARCHAR(256) and not NULL
+CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL, state_id INT NOT NULL,
+    FOREIGN KEY(state_id) REFERENCES states(id), name VARCHAR(256) NOT NULL);
+    
