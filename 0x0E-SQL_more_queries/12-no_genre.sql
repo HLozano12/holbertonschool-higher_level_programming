13331a5a063b4d439d171ccdf08afc4e0478a95c
HLozano12 <HLR8503@gmail.com>
2021-05-28 01:12:26 +0000

    TYPOSgit add 12-no_genre.sql git add 12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -2,7 +2,7 @@
 -- show genre linked shows
 -- display tv_shows.title - tv_show_genres.genre_id
 -- ONLY ONE SELECT
-SELECT tv_shows.title, tv_show_genre.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_show_genres
 RIGHT OUTER JOIN tv_shows
 ON tv_shows.id = tv_show_genres.show_id
