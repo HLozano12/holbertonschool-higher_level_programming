8cadda6b73f8615cdfeb9b4751f8669443d66fa9
HLozano12 <HLR8503@gmail.com>
2021-05-28 00:10:45 +0000

    OrderBY

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,8 @@
+-- import a DB dump from hbtn_0d_tvshows to download server
+-- list all shows contained in hbtn_0d_tvshows with at least one link genre
+-- sort in ASC by tv_shows.title & tv_show_genre.genre_id
+SELECT tv_shows.title, tv_show_genre.genre_id
+FROM tv_shows
+INNER JOIN tv_shows_genres
+ON tv_shows_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title ASC, tv_shows_genres.genre_id ASC
