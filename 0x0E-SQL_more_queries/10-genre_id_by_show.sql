db09fcd679895f9d9df4fa1a36501cc09b57c92c
HLozano12 <HLR8503@gmail.com>
2021-05-28 00:12:19 +0000

    TypeAGAIN!

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,7 @@
 -- import a DB dump from hbtn_0d_tvshows to download server
 -- list all shows contained in hbtn_0d_tvshows with at least one link genre
 -- sort in ASC by tv_shows.title & tv_show_genre.genre_id
-SELECT tv_shows.title, tv_show_genre.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_shows_genres
 ON tv_shows_genres.show_id = tv_shows.id
