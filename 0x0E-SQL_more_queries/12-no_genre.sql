bda76e45114c27b1195b67a7a1aeee841fb6b475
HLozano12 <HLR8503@gmail.com>
2021-05-28 01:04:41 +0000

    join

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,10 @@
+-- Similar to 11
+-- show genre linked shows
+-- display tv_shows.title - tv_show_genres.genre_id
+-- ONLY ONE SELECT
+SELECT tv_shows.title, tv_show_genre.genre_id
+FROM tv_show_genres
+RIGHT OUTER JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+WHERE show_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
