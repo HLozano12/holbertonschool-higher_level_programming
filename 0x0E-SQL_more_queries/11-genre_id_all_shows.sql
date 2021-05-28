5685d521347102ae503fa0d618a18572d04178a3
HLozano12 <HLR8503@gmail.com>
2021-05-28 00:42:27 +0000

    One Select

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,10 @@
+-- DUMP DB just like task 10
+-- list shws containing DB hbtn_0d_tvshows
+-- only one SELECT statement...yay
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.show_id
+LEFT JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
