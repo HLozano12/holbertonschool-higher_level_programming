9e60fcc088ee75c16685bac3fff8c1b79de4f9d3
HLozano12 <HLR8503@gmail.com>
2021-05-28 00:14:03 +0000

    Right or Inner

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,6 +3,6 @@
 -- sort in ASC by tv_shows.title & tv_show_genre.genre_id
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-INNER JOIN tv_shows_genres
+RIGHT JOIN tv_shows_genres
 ON tv_shows_genres.show_id = tv_shows.id
 ORDER BY tv_shows.title ASC, tv_shows_genres.genre_id ASC
