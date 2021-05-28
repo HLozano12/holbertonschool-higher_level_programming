a30d92521e5c5c9ddeea8244f21bd591a805f83e
HLozano12 <HLR8503@gmail.com>
2021-05-28 00:18:24 +0000

    Typos...again

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -4,5 +4,5 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 RIGHT JOIN tv_shows_genres
-ON tv_shows_genres.show_id = tv_shows.id
-ORDER BY tv_shows.title ASC, tv_shows_genres.genre_id ASC
+ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC
