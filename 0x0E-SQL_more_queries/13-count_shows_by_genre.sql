ce24e7625581452634e8544031e1009f187aa472
HLozano12 <HLR8503@gmail.com>
2021-05-28 01:24:17 +0000

    GROUP ORDER

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- Each record should display: <TV Show genre> - <Number of shows linked to this genre>
+-- First column must be called genre
+-- You can use only one SELECT statement
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres
+ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY tv_genres.genre_id
+ORDER BY number_of_shows DESC;
