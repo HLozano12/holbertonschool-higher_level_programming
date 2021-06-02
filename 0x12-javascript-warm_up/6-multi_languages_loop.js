19865f0ad1806f0a4116a58504d34c048f5b8d60
HLozano12 <HLR8503@gmail.com>
2021-06-02 23:03:15 +0000

    Array3Ling

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/*
+Print 3 lines using array of strings
+*/
+
+const MyArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let h = 0; h < MyArray.length; h++) {
+  console.log(MyArray[h]);
+}
