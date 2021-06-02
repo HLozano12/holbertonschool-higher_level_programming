6cccb27a581f8ab706e424637e5dd759a3a2ebe5
HLozano12 <HLR8503@gmail.com>
2021-06-02 20:19:03 +0000

    Update Semi

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,12 +5,10 @@ print a message depending of the number of arguments passed
 
 const MyArgs = process.argv.slice(2);
 
-if (MyArgs.length === 0){
-    console.log('No argument');
+if (MyArgs.length === 0) {
+  console.log('No argument');
+} else if (MyArgs.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
-    else if (MyArgs.length === 1){
-    console.log('Argument found');
-    }
-    else {
-    console.log('Arguments found');
-    }
