60d86b47f1e85820e0e592193451eead010e32ca
HLozano12 <HLR8503@gmail.com>
2021-06-03 21:22:18 +0000

    Double otate

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+/*
+Like previous task
+adding
+Create an instance method called rotate() that exchanges the width and the height of the rectangle
+Create an instance method called double() that multiples the width and the height of the rectangle by 2
+*/
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let h = 0; h < this.height; h++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
