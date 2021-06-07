97bc1c7542cdcc3d586471c97800db5002334522
HLozano12 <HLR8503@gmail.com>
2021-06-07 04:59:10 +0000

    At the buzzer

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*
+function that returns the addition of 2 integers.
+Visable outside
+function must be name add
+no var
+*/
+
+exports.add = function (h, l) {
+  return (h + l);
+};
