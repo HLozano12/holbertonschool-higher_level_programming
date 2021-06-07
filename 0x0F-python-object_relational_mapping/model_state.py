62f423da74bc0d4b3e62cdcdfe9d34d38563d353
HLozano12 <HLR8503@gmail.com>
2021-06-07 18:36:23 +0000

    Why No work

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,20 +2,19 @@
 """
 python file that contains the class definition of a State
 and an instance Base = declarative_base()
--State class that inherits from Base
--Links to MySQL Table States
--Class atty id to rep auto gen, uniq int, cannot be null and is pk
+State class that inherits from Base
+Links to MySQL Table States
+Class atty id to rep auto gen, uniq int, cannot be null and is pk
 """
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declaratibe_base()
-""" Comment on module missing"""
+Base = declarative_base()
 
 
 class State(Base):
     """Module comment for class inherit"""
     __tablename__ = "states"
-    id = Column(Integer, pk=True, Uniq=True, Nully=True)
-    name = Column(String(128), Nully=False)
+    id = Column(Integer, primary_key=True, unique=True, nullable=True)
+    name = Column(String(128), nullable=False)
