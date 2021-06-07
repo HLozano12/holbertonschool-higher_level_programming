4899609ea4b2b765b86d6f57c15ef7f6f3e13fe4
HLozano12 <HLR8503@gmail.com>
2021-06-07 02:40:58 +0000

    semi error fix

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -5,10 +5,10 @@ updating script in Main file
 */
 
 const myObject = {
-    type: 'object',
-    value: 12
-  };
-  console.log(myObject);
-  /* my code */
-  myObject.value = 89
-  console.log(myObject);
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+/* my code */
+myObject.value = 89;
+console.log(myObject);
