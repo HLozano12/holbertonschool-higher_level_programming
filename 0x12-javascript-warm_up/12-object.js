387736307325a48dfb906c979a451b00160a822b
HLozano12 <HLR8503@gmail.com>
2021-06-07 02:40:05 +0000

    Change Value

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/*
+replace value 12 with 89
+updating script in Main file
+*/
+
+const myObject = {
+    type: 'object',
+    value: 12
+  };
+  console.log(myObject);
+  /* my code */
+  myObject.value = 89
+  console.log(myObject);
