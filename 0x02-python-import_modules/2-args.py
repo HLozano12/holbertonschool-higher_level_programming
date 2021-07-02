215b8e8315a017f4d9c5c93c4bd1ca5f8d3eb294
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-01 19:59:49 -0500

    Update typos

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
 	if h == 1:
 		print("0 arguments.")
 	elif h == 2:
-		print("1 argument")
+		print("1 argument:")
 	else:
 		print("{} arguments:".format(h - 1))
 	for l in range(1, h):
-		print("{}: {}".format(i, sys.argv[l]))
+		print("{}: {}".format(l, sys.argv[l]))
