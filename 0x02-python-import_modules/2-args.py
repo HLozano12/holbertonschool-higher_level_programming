62f02d3dea45084011e0b816c76e1c8d2174642b
Hector Lozano <68207606+HLozano12@users.noreply.github.com>
2021-07-01 20:11:56 -0500

    WOrk NOw?

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
-#!/usr/bin/python3
+ #!/usr/bin/python3
 import sys
-if __name__ == "__main__":
-	h = len(sys.argv)
-	if h == 1:
-		print("0 arguments.")
-	elif h == 2:
-		print("1 argument:")
-	else:
-		print("{} arguments:".format(h - 1))
-	for l in range(1, h):
-		print("{}: {}".format(l, sys.argv[l]))
+if __name__ == '__main__':
+    h = len(sys.argv)
+    if h == 1:
+        print("0 arguments.")
+    elif h == 2:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(h - 1))
+    for l in range(1, h):
+        print("{}: {}".format(l, sys.argv[l]))
